27a4b9a12113a738cd0f83131feda494e8b4f661
Hari <harimurali10a@gmail.com>
2026-04-23 19:26:54 +0530

    Cleanup: Remove temporary test files

--- backend/dns_test.js
+++
@@ -1,8 +0,0 @@
-const dns = require('dns');
-dns.resolveSrv('_mongodb._tcp.cluster0.set9j2m.mongodb.net', (err, addresses) => {
-    if (err) {
-        console.error('SRV Resolution Error:', err);
-        return;
-    }
-    console.log('SRV Addresses:', addresses);
-});
